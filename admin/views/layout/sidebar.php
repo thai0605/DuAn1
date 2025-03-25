a401abe718f2843ccfcbc07d1a1003e6b5f59bde
ThinhNT5 <thinhntph52573@gmail.com>
2025-03-25 21:43:51 +0700

    Cập Nhật Code

---
+++ admin/views/layout/sidebar.php
@@ -0,0 +1,21 @@
+<aside class="left-sidebar" data-sidebarbg="skin5">
+    
+    <div class="scroll-sidebar">
+      
+        <nav class="sidebar-nav">
+            <ul id="sidebarnav" class="p-t-30">
+                <li class="sidebar-item"> <a class="sidebar-link waves-effect waves-dark sidebar-link" href="?act=/" aria-expanded="false"><i class="menu-icon fa fa-home" style="font-size: 20px; margin-right: 10px;"></i><span class="hide-menu">DashBoard</span></a></li>
+                <li class="sidebar-item"> <a class="sidebar-link waves-effect waves-dark sidebar-link" href="?act=listdm" aria-expanded="false"><i class="menu-icon fa fa-th-large" style="font-size: 18px; margin-right: 10px;"></i><span class="hide-menu">Danh Mục Sản Phẩm </span></a></li>
+                <li class="sidebar-item"> <a class="sidebar-link waves-effect waves-dark sidebar-link" href="?act=san-pham" aria-expanded="false"><i class="menu-icon fa fa-book" style="font-size: 18px; margin-right: 10px;"></i><span class="hide-menu">Sản phẩm</span></a></li>
+                <li class="sidebar-item"> <a class="sidebar-link waves-effect waves-dark sidebar-link" href="?act=order" aria-expanded="false"><i class="menu-icon fa fa-list-alt" style="font-size: 18px; margin-right: 10px;"></i><span class="hide-menu">Danh sách đơn hàng </span></a></li>
+                <li class="sidebar-item"> <a class="sidebar-link waves-effect waves-dark sidebar-link" href="?act=khuyen-mai" aria-expanded="false"><i class="menu-icon fa fa-calendar" style="font-size: 18px; margin-right: 10px;"></i><span class="hide-menu">Quản Lý Khuyễn Mãi </span></a></li>
+                <li class="sidebar-item"> <a class="sidebar-link waves-effect waves-dark sidebar-link" href="?act=binh-luan" aria-expanded="false"><i class="menu-icon fa fa-comments" style="font-size: 20px; margin-right: 10px;"></i><span class="hide-menu">Quản Lý Bình Luận </span></a></li>
+                <li class="sidebar-item"> <a class="sidebar-link waves-effect waves-dark sidebar-link" href="?act=danh-gia" aria-expanded="false"><i class="menu-icon fa fa-star" style="font-size: 20px; margin-right: 10px;"></i><span class="hide-menu">Quản Lý Đánh Giá </span></a></li>
+                <li class="sidebar-item"> <a class="sidebar-link waves-effect waves-dark sidebar-link" href="?act=giao-dien" aria-expanded="false"> <i class="menu-icon fa fa-picture" style="font-size: 20px; margin-right: 10px;"></i><span class="hide-menu">Quản Lý Banner </span></a></li>
+                <li class="sidebar-item"> <a class="sidebar-link waves-effect waves-dark sidebar-link" href="?act=user" aria-expanded="false"><i class="menu-icon fa fa-users" style="font-size: 18px; margin-right: 10px;"></i><span class="hide-menu">Quản Lý Tài Khoản </span></a></li>
+            </ul>
+        </nav>
+       
+    </div>
+  
+</aside>
---
+++ commons/env.php
@@ -0,0 +1,14 @@
+<?php
+
+define('BASE_URL' , 'http://localhost/DuAn1-main/');
+define('BASE_URL_ADMIN' , 'http://localhost/DuAn1-main//admin/');
+
+
+
+define('DB_HOST', 'localhost');
+define('DB_PORT', 3306);
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_NAME', 'duan11');
+
+define('PATH_ROOT', __DIR__ . '/../');
