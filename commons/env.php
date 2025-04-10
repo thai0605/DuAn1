38a2bd5685430d1d73fb71e83a3e8da70353ff49
BuiMinhCuong <cuongbmph50042@gmail.com>
2025-04-10 12:14:48 +0700

    sua/database

--- commons/env.php
+++ commons/env.php
@@ -9,6 +9,6 @@ define('DB_HOST', 'localhost');
 define('DB_PORT', 3306);
 define('DB_USERNAME', 'root');
 define('DB_PASSWORD', '');
-define('DB_NAME', 'duan1main');
+define('DB_NAME', 'duan1');
 
 define('PATH_ROOT', __DIR__ . '/../');
